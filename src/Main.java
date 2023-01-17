b2aa3016877452c5b3f9055367bec990414c60bf
saad idrharnane <saad.idrharnane.saad@etu.educ-valadon-limoges.fr>
2023-01-17 15:35:38 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    { /*EX2*/
+
+        /* float prix1,prix2,prix3,total;
+        prix1 = 11.5F;
+        prix2 = 8.5F;
+        prix3 = 4;
+        total=prix1+prix2+prix3;
+ System.out.println(total);*/
+        /*EX3*/
+        /*  double nombre = 3.14;
+
+        System.out.println(nombre);
+        int entier;
+        entier = (int)nombre;
+        System.out.println(entier);*/
+        /*EX4*/
+       /* Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Saisissez votre prénom");
+
+        String prenom = sc.next();
+        System.out.println("Saisissez votre nom");
+        String nom = sc.next();
+        System.out.println("Bonjour "+nom+" "+prenom);*/
+        /*EX5*/
+        /*System.out.println("saisir un entier ");
+        Scanner sc = new Scanner(System.in);
+        int nbri =sc.nextInt();
+        System.out.println(nbri);
+        System.out.println("saisir un float ");
+        float nbrf =sc.nextFloat();
+        System.out.println(nbrf);
+        System.out.println("saisir un double ");
+        double nbrd =sc.nextDouble();
+        System.out.println(nbrd);*/
+
+        /*EX6*/
+        Scanner sc = new Scanner(System.in);
+        System.out.println("saisiez un text");
+        String saisie = sc.next();
+        System.out.println(saisie);
+    }
+}
